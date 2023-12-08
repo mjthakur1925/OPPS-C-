ac5779259bcef754e372f6e56c7f37ead94a90b4
manisha thakur <manishathakur1925@gmail.com>
2023-12-08 21:58:31 +0530

    first commit

---
+++ class.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+class Student{
+    public:
+    string name;
+    int age;
+    bool gender;
+};
+
+int main()
+{
+    Student Manisha;
+    Manisha.name = "Manisha";
+    return 0;
+}
